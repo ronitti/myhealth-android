43b6e1409a50bc4bc64a78f547732a1188b37fdc
Ivo Mendes <ivopmendez@gmail.com>
2019-04-25 15:28:31 -0300

    Fix conflict package names

--- app/src/androidTest/java/myhealth/ufscar/br/myhealth/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/myhealth/ufscar/br/myhealth/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("myheatlh.ufscar.br.myhealth", appContext.getPackageName());
+        assertEquals("myhealth.ufscar.br.myhealth", appContext.getPackageName());
     }
 }
--- app/src/main/java/myhealth/ufscar/br/myhealth/ui/RegisterActivity.java
+++ app/src/main/java/myhealth/ufscar/br/myhealth/ui/RegisterActivity.java
@@ -1,15 +1,61 @@
 package myhealth.ufscar.br.myhealth.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import myhealth.ufscar.br.myhealth.R;
+import myhealth.ufscar.br.myhealth.ui.fragments.AccessDataFragment;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private static final int NUM_PAGES = 7;
+    private ViewPager mPager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mPager = findViewById(R.id.pager);
+        pagerAdapter = new AccessDataAdapter(getSupportFragmentManager());
+        mPager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class AccessDataAdapter extends FragmentStatePagerAdapter {
+        public AccessDataAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new AccessDataFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
